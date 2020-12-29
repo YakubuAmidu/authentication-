3e1aa2cf2457ff73b01bc5e57b25453f07e583c3
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-29 07:07:42 -0800

    Initial commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {TextInput} from 'react-native';
 import {Button, Card, CardSection} from './common';
 
 class LoginForm extends Component {
+  state = {
+    text: ' ',
+  };
+
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChange={(text) => this.setState({text})}
+            style={{height: 20, width: 100}}
+          />
+        </CardSection>
         <CardSection />
 
         <CardSection>
