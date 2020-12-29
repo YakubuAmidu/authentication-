b500777fbb3849029450b8e8588a438c23183233
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-29 05:50:31 -0800

    Initial commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Button, Card, CardSection} from 'react-native';
+
+class LoginForm extends Component {
+  render() {
+    return <View />;
+  }
+}
+
+export default LoginForm;
